bd21632504b30059945efecc1b1ced9e0aceef3b
freedom <stefano.vettor@mac.com>
2016-01-31 17:44:41 +0100

    added rounding on numbers coming from the sensors

--- sensehatsensor.py
+++ sensehatsensor.py
@@ -10,9 +10,9 @@ class SenseHatSensor(ISensor):
     def update_status(self, status):
         global sense_hat
 
-        status["temperature"] = sense_hat.temperature
+        status["temperature"] = round(sense_hat.temperature, 2)
         status["temperature_unit"] = "C"
-        status["humidity"] = sense_hat.humidity
+        status["humidity"] = round(sense_hat.humidity, 2)
         status["humidity_unit"] = "%"
-        status["pressure"] = sense_hat.pressure
+        status["pressure"] = round(sense_hat.pressure, 2)
         status["pressure_unit"] = "mbar"
