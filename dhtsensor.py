7458663d9c39e784068d52023673a0dc4d233a44
freedom <stefano.vettor@mac.com>
2016-03-02 20:18:12 +0100

    improving the DHT sensor... better response time leveraging cached data

--- dhtsensor.py
+++ dhtsensor.py
@@ -15,8 +15,25 @@ class DHTSensor(ISensor):
         else:
             self._sensor_model = Adafruit_DHT.DHT11
 
+        self._old_temperature = 0
+        self._old_humidity = 0
+
     def update_status(self, status: Status):
-        humidity, temperature = Adafruit_DHT.read_retry(self._sensor_model, self._pin)
+        humidity, temperature = Adafruit_DHT.read(self._sensor_model, self._pin)
+        # the sensor may fail and since I do not want to use the read_retry function (waste too much time)
+        # I store old values for humidity and temperature and if a sensor read fails the old ones are returned.
+        # This is a good solution in case of short sampling intervals. If the sampling interval is long (above the
+        # the minute) it's better to use the read_retry function instead.
+        if humidity is None:
+            humidity = self._old_humidity
+        else:
+            self._old_humidity = humidity
+
+        if temperature is None:
+            temperature = self._old_temperature
+        else:
+            self._old_temperature = temperature
+
         if "temperature" in self.sensors:
             status["temperature"] = round(temperature, 2)
         if "humidity" in self.sensors:
